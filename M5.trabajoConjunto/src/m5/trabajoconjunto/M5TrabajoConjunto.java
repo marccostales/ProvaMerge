0c12b6aa13083956db9d1ae47b5fef2dc88109e3
marccostales <102044117+marccostales@users.noreply.github.com>
2022-04-04 15:49:08 +0200

    Add files via upload

---
+++ M5.trabajoConjunto/src/m5/trabajoconjunto/M5TrabajoConjunto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m5.trabajoconjunto;
+
+/**
+ *
+ * @author marco
+ */
+public class M5TrabajoConjunto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("D");
+        System.out.println("E");
+        
+        
+    }
+    
+}
